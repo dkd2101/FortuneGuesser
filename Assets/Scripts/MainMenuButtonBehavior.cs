44d25c31acd83df18804cebc526a910149fff7a9
Linwood <74383923+Blais3D@users.noreply.github.com>
2024-03-24 15:05:13 -0400

    Linwood grinds (#9)
    * Controls done
    
    * fadeout working
    
    * oops

--- Assets/Scripts/MainMenuButtonBehavior.cs
+++ Assets/Scripts/MainMenuButtonBehavior.cs
@@ -6,9 +6,18 @@ using UnityEngine.SceneManagement;
 public class MainMenuButtonBehavior : MonoBehaviour
 {
     public string _toSceneName;
+    public int _toSceneNumber;
 
     public void OnClick()
     {
-        SceneManager.LoadScene(_toSceneName);
+        GameObject temp = GameObject.FindGameObjectWithTag("LevelManager");
+        if (temp != null)
+        {
+            temp.GetComponent<LevelManager>().LoadAScene(_toSceneNumber);
+        }
+        else
+        {
+            SceneManager.LoadScene(_toSceneName);
+        }
     }
 }
